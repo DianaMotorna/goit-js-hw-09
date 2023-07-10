bdfcbfad8f9f19aee5bf9da6c39a93edf43455dc
DianaMotorna <124207230+DianaMotorna@users.noreply.github.com>
2023-07-10 22:41:25 +0300

    formatting 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,28 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-  }
-  
-  let intervalId;
-  
-  function startChangingColor() {
-    const body = document.querySelector('body');
-    intervalId = setInterval(function () {
-      body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
-  
-    document.querySelector('[data-start]').disabled = true;
-  }
-  
-  function stopChangingColor() {
-    clearInterval(intervalId);
-    document.querySelector('[data-start]').disabled = false;
-  }
-  
-  document.querySelector('[data-start]').addEventListener('click', startChangingColor);
-  document.querySelector('[data-stop]').addEventListener('click', stopChangingColor);
-  
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+let intervalId;
+
+function startChangingColor() {
+  const body = document.querySelector('body');
+  intervalId = setInterval(function () {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  document.querySelector('[data-start]').disabled = true;
+}
+
+function stopChangingColor() {
+  clearInterval(intervalId);
+  document.querySelector('[data-start]').disabled = false;
+}
+
+document
+  .querySelector('[data-start]')
+  .addEventListener('click', startChangingColor);
+document
+  .querySelector('[data-stop]')
+  .addEventListener('click', stopChangingColor);
